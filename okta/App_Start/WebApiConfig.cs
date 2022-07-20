f3960751a58b74bf8120a5a1e1b80149b94b70ec
Rituraj <returajvaishnav2@gamil.com>
2022-07-20 13:30:38 +0530

    [fix]-add origin

--- okta/App_Start/WebApiConfig.cs
+++ okta/App_Start/WebApiConfig.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
-
+using System.Web.Http.Cors;
 namespace okta
 {
     public static class WebApiConfig
@@ -10,9 +10,12 @@ namespace okta
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            var cors = new EnableCorsAttribute("http://localhost:4200", "*", "*");
+            config.EnableCors(cors);
 
             // Web API routes
             config.MapHttpAttributeRoutes();
+            
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
